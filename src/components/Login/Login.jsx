aaa16d2d53d3911adc9472f6ce477c8e5fc4ae2e
mahmudhasan123 <mahmudcodes@gmail.com>
2025-05-06 08:00:02 +0600

    add register and login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+  };
 
-export default Login
+  return (
+    <div className="card mx-auto mt-10 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-2xl font-bold">Login now!</h1>
+        <form onSubmit={handleLogin} className="fieldset">
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" name="email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" name="password" />
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </form>
+        <p>
+          New to this website? Please{" "}
+          <Link to="/register" className="text-blue-500 underline">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React from "react";
+import { Link } from "react-router";
+import { auth } from "../firebase.init";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
 
-export default Register
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log("Result: ", result);
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+
+  return (
+    <div className="card mx-auto mt-10 bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-2xl font-bold">Create Account!</h1>
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" className="input" placeholder="Name" name="name" />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            name="email"
+          />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            name="password"
+          />
+          <button className="btn btn-neutral mt-4">Sign Up</button>
+        </form>
+        <p>
+          Already have an Account? Please{" "}
+          <Link to="/login" className="text-blue-500 underline">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
